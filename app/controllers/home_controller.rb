ee9fac7474c2a391aa111e40119ba9763088c1ba
tclewis91 <tclewis91@gmail.com>
2015-07-15 12:56:16 -0500

    materialize

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    return 'sup'
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'rest_client'
+
+res = RestClient::Request.execute(:url => 'https://www.kimonolabs.com/api/bd7d2c66?apikey=N5gmFp4txNaM14V0074wiv7FAtDuRr1L', :method => :get, :verify_ssl => false)
+hsh = JSON.parse(res)
+hsh[‘results’][‘collection1’].each do |x|
+
+  def change_name(collection)
+   if row['scientific_name']
+      return row['scientific_name']
+   else
+      return row['genus']['text'], row['species']['text']
+    end
+
+  Snake.create({ scientific_name: ['scientific_name’],
+                  name: ['name'],
+                  image: ['name']['href’] })
+    end
+
+  def name_match(collection)
+    Snake.select(:scientific_name).group(:scientific_name).having("count(*) > 1")
+  end
+
+  def snake(collection)
+
+def
+
+
+
