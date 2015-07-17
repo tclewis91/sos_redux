d7dbcdb0210f5053cb38b7d6711d5de5a7186f4f
tclewis91 <tclewis91@gmail.com>
2015-07-17 13:40:12 -0500

    seed file httparty gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-require 'rest_client'
+require 'httparty'
 
 URL = 'https://www.kimonolabs.com/api/' + ENV["kimono_labs_api_id"] + '?apikey=' + ENV["kimono_labs_api_key"]
 puts URL
 
-res = RestClient::Request.execute(:url => URL, :method => :get, :verify_ssl => false)
+res = HTTPARTY::Request.execute(:url => URL, :method => :get, :verify_ssl => false)
 hsh = JSON.parse(res)
 hsh['results']['Band Snakes'].each do |row|
   Snake.create ({ name: row['name'],
@@ -11,49 +11,48 @@ hsh['results']['Band Snakes'].each do |row|
                   image: row['imge']
                         })
 
+# URL = 'https://www.kimonolabs.com/api/' + ENV["kimono_labs_api_id"] + '?apikey=' + ENV["kimono_labs_api_key"]
+# puts URL
 
-URL = 'https://www.kimonolabs.com/api/' + ENV["kimono_labs_api_id"] + '?apikey=' + ENV["kimono_labs_api_key"]
-puts URL
+# res = HTTPARTY::Request.execute(:url => URL, :method => :get, :verify_ssl => false)
+# hsh = JSON.parse(res)
+# hsh['results']['Stripes Sankes'].each do |row|
 
-res = RestClient::Request.execute(:url => URL, :method => :get, :verify_ssl => false)
-hsh = JSON.parse(res)
-hsh['results']['Stripes Sankes'].each do |row|
+# URL = 'https://www.kimonolabs.com/api/' + ENV["kimono_labs_api_id"] + '?apikey=' + ENV["kimono_labs_api_key"]
+# puts URL
 
-URL = 'https://www.kimonolabs.com/api/' + ENV["kimono_labs_api_id"] + '?apikey=' + ENV["kimono_labs_api_key"]
-puts URL
+# res = HTTPARTY::Request.execute(:url => URL, :method => :get, :verify_ssl => false)
+# hsh = JSON.parse(res)
+# hsh['results']['Solid Snakes'].each do |row|
 
-res = RestClient::Request.execute(:url => URL, :method => :get, :verify_ssl => false)
-hsh = JSON.parse(res)
-hsh['results']['Solid Snakes'].each do |row|
+# URL = 'https://www.kimonolabs.com/api/' + ENV["kimono_labs_api_id"] + '?apikey=' + ENV["kimono_labs_api_key"]
+# puts URL
 
-URL = 'https://www.kimonolabs.com/api/' + ENV["kimono_labs_api_id"] + '?apikey=' + ENV["kimono_labs_api_key"]
-puts URL
-
-res = RestClient::Request.execute(:url => URL, :method => :get, :verify_ssl => false)
-hsh = JSON.parse(res)
-hsh['results']['Blotches Snakes'].each do |row|
-
-
-def change_name(row)
-  if row['scientific_name']
-     scientific_name = row['scientific_name']
-  else
-    scientific_name = "#{row['genus']['text']} #{row['species']['text']}"
-  end
-  if Snake.where(scientific_name: scientific_name).count.zero?
-    Snake.create({ scientific_name: scientific_name,
-                   name: row['name'],
-                  image: row['name']['href'],
-                  characteristic: row['url'] })
-    end
-  end
-end
+# res = HTTPARTY::Request.execute(:url => URL, :method => :get, :verify_ssl => false)
+# hsh = JSON.parse(res)
+# hsh['results']['Blotches Snakes'].each do |row|
 
 
+# def change_name(row)
+#   if row['scientific_name']
+#      scientific_name = row['scientific_name']
+#   else
+#     scientific_name = "#{row['genus']['text']} #{row['species']['text']}"
+#   end
+#   if Snake.where(scientific_name: scientific_name).count.zero?
+#     Snake.create({ scientific_name: scientific_name,
+#                    name: row['name'],
+#                   image: row['name']['href'],
+#                   characteristic: row['url'] })
+#     end
+#   end
+# end
 
-name
-color
-api
-image
+
+
+# name
+# color
+# api
+# image
 
 
