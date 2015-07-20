e8fcd943d8f4588ac03e243b7e41162cf553f750
tclewis91 <tclewis91@gmail.com>
2015-07-20 14:24:34 -0500

    added patch to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,30 @@
 Rails.application.routes.draw do
   resources :firstaids
-  resources :snakes
+
+  resources :snakes do
+    member do
+      patch 'match_red'
+      patch 'match_blue'
+      patch 'match_yellow'
+      patch 'match_white'
+      patch 'match_black'
+      patch 'match_orange'
+      patch 'match_green'
+      patch 'match_band'
+      patch 'match_stripes'
+      patch 'match_solid'
+      patch 'match_blotch'
+    end
+  end
+
   resources :abouts
   devise_for :users
 
   resources :posts do
   resources :comments
   end
+
+  resources :user
   resources :home
 
   root to: "home#index"
