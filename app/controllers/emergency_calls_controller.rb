698e1ad1979b0c9917a6b3bf9df9602b7deba5f9
tclewis91 <tclewis91@gmail.com>
2015-07-21 07:13:47 -0500

    altered facetime

--- app/controllers/emergency_calls_controller.rb
+++ app/controllers/emergency_calls_controller.rb
@@ -11,46 +11,4 @@ class EmergencyCallsController < ApplicationController
   # GET /emergency_calls/1.json
   def show
   end
-
-  tel_to '+1 318 225-0495'
-
-  # GET /emergency_calls/new
-  def new
-    @emergency_call = EmergencyCall.new
-  end
-
-  # GET /emergency_calls/1/edit
-  def edit
-  end
-
-  # POST /emergency_calls
-  # POST /emergency_calls.json
-  def create
-    @emergency_call = EmergencyCall.new(emergency_call_params)
-
-    respond_to do |format|
-      if @emergency_call.save
-        format.html { redirect_to @emergency_call, notice: 'Emergency call was successfully created.' }
-        format.json { render :show, status: :created, location: @emergency_call }
-      else
-        format.html { render :new }
-        format.json { render json: @emergency_call.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /emergency_calls/1
-  # PATCH/PUT /emergency_calls/1.json
-  def update
-    respond_to do |format|
-      if @emergency_call.update(emergency_call_params)
-        format.html { redirect_to @emergency_call, notice: 'Emergency call was successfully updated.' }
-        format.json { render :show, status: :ok, location: @emergency_call }
-      else
-        format.html { render :edit }
-        format.json { render json: @emergency_call.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-
+end
